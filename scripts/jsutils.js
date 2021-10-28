6c17f5799bd5c3f595f46661782af51892f652ee
sixtysixgames <nosbig.kram@hotmail.co.uk>
2021-10-28 18:04:29 +0000

    formatting

--- scripts/jsutils.js
+++ scripts/jsutils.js
@@ -20,9 +20,9 @@
 
 function isValid(variable) {
     return ((variable !== null) &&
-            (variable !== undefined) &&
-            (variable === variable)); // This is a way to test for NaN that
-            // isn't subject to the unexpected behavior of isNaN().
+        (variable !== undefined) &&
+        (variable === variable)); // This is a way to test for NaN that
+    // isn't subject to the unexpected behavior of isNaN().
 }
 
 // Returns the variable if it's valid, otherwise the default value (or "")
@@ -34,18 +34,18 @@ function ifValid(variable, defVal) {
 // Evaluates and returns variable if it's a function, otherwise just returns it.
 // Passes surplus arguments on to the function.
 // TODO: xxx argument forwarding needs testing.
-function valOf (variable) { 
+function valOf(variable) {
     if (typeof variable == "function") {
-        return variable.apply(this, Array.prototype.slice.call(arguments,1));
+        return variable.apply(this, Array.prototype.slice.call(arguments, 1));
     } else {
         return variable;
     }
 }
 
 function bake_cookie(name, value) {
-    var exdate=new Date();
+    var exdate = new Date();
     exdate.setDate(exdate.getDate() + 30);
-    var cookie = [name, "=", JSON.stringify(value),"; expires=.", exdate.toUTCString(), "; domain=.", window.location.host.toString(), "; path=/;"].join("");
+    var cookie = [name, "=", JSON.stringify(value), "; expires=.", exdate.toUTCString(), "; domain=.", window.location.host.toString(), "; path=/;"].join("");
     document.cookie = cookie;
 }
 function read_cookie(name) {
@@ -57,43 +57,37 @@ function read_cookie(name) {
 
 // Calculates the summation of elements (n...m] of the arithmetic sequence
 // with increment "incr".
-function calcArithSum(incr,n,m)
-{
+function calcArithSum(incr, n, m) {
     // Default to just element n+1, if m isn't given.
     if (m === undefined) { m = n + 1; }
-    return (m-n)*((n*incr)+((m-1)*incr))/2;
+    return (m - n) * ((n * incr) + ((m - 1) * incr)) / 2;
 }
 
 // Search for the largest integer X that generates func(X) < limitY.
 // func should be a continuous increasing numeric function.
 //xxx This would probably be more elegant written recursively.
-function logSearchFn(func, limitY)
-{
+function logSearchFn(func, limitY) {
     var minX = 0;
     var maxX = 0;
     var curX = 0;
     var curY;
 
     // First, find an upper bound.
-    while ((curY = func(maxX)) <= limitY)
-    {
+    while ((curY = func(maxX)) <= limitY) {
         minX = maxX;  // Previous was too low
         maxX = maxX ? maxX * 2 : (maxX + 1);
     }
     // Invariant:  minX <= desired X < maxX
 
     // Now binary search the range.
-    while (maxX - minX > 1)
-    {
-        curX = Math.floor((maxX + minX)/2); // Find midpoint
+    while (maxX - minX > 1) {
+        curX = Math.floor((maxX + minX) / 2); // Find midpoint
         curY = func(curX);
 
-        if (curY <= limitY)
-        {
+        if (curY <= limitY) {
             minX = curX; // Under limit; becomes new lower bound.
         }
-        else
-        {
+        else {
             maxX = curX; // Over limit; becomes new upper bound.
         }
     }
@@ -103,25 +97,23 @@ function logSearchFn(func, limitY)
 
 // Recursively merge the properties of one object into another.
 // Similar (though not identical) to jQuery.extend()
-function mergeObj(o1, o2) 
-{
+function mergeObj(o1, o2) {
     var i;
-
     if (o2 === undefined) { return o1; }
 
     // If either one is a non-object, just clobber o1.
-    if ((typeof(o2) != "object") || (o1 === null) ||
-        (typeof(o1) != "object") || (o2 === null))
-    {
+    if ((typeof (o2) != "object") || (o1 === null) ||
+        (typeof (o1) != "object") || (o2 === null)) {
         o1 = o2;
         return o1;
     }
 
     // Both are non-null objects.  Copy o2's properties to o1.
-    for (i in o2) { if (o2.hasOwnProperty(i)) 
-    {
-        o1[i] = mergeObj(o1[i], o2[i]);
-    }}
+    for (i in o2) {
+        if (o2.hasOwnProperty(i)) {
+            o1[i] = mergeObj(o1[i], o2[i]);
+        }
+    }
 
     return o1;
 }
@@ -130,23 +122,20 @@ function mergeObj(o1, o2)
 // Also searches up the DOM tree on lookups, to mimic inheritance.
 // Pass 'value' to set the value, otherwise returns the value.
 // Returns "true" and "false" as actual booleans.
-function dataset(elem,attr,value)
-{
-    if (value !== undefined) { return elem.setAttribute("data-"+attr,value); }
+function dataset(elem, attr, value) {
+    if (value !== undefined) { return elem.setAttribute("data-" + attr, value); }
 
     var val = null;
-    for (var i = elem; i; i = i.parentNode)
-    {
+    for (var i = elem; i; i = i.parentNode) {
         if (i.nodeType != Node.ELEMENT_NODE) { continue; }
-        val = i.getAttribute("data-"+attr); 
+        val = i.getAttribute("data-" + attr);
         if (val !== null) { break; }
     }
-    return (val=="true")?true:(val=="false")?false:val;
+    return (val == "true") ? true : (val == "false") ? false : val;
 }
 
 // Probabilistic rounding function
-function rndRound(num)
-{
+function rndRound(num) {
     var baseVal = Math.floor(num);
     return baseVal + ((Math.random() < (num - baseVal)) ? 1 : 0);
 }
@@ -154,30 +143,26 @@ function rndRound(num)
 // Copy properties from to dest from src
 // If 'names' array supplied, only copies the named properties
 // If 'deleteOld' is true, deletes the properties from the old object
-function copyProps(dest,src,names,deleteOld)
-{
+function copyProps(dest, src, names, deleteOld) {
     if (!(names instanceof Array)) { names = Object.getOwnPropertyNames(src); }
     if (!isValid(deleteOld)) { deleteOld = false; }
 
-    names.forEach(function(elem){
+    names.forEach(function (elem) {
         if (!src.hasOwnProperty(elem)) { return; }
         // This syntax is needed to copy get/set properly; you can't just use '='.
-        Object.defineProperty(dest,elem,Object.getOwnPropertyDescriptor(src,elem));
+        Object.defineProperty(dest, elem, Object.getOwnPropertyDescriptor(src, elem));
         if (deleteOld) { delete src[elem]; }
     });
 }
 
 // Delete the specified named cookie
-function deleteCookie(cookieName)
-{
+function deleteCookie(cookieName) {
     document.cookie = [cookieName, "=; expires=Thu, 01-Jan-1970 00:00:01 GMT; path=/; domain=.", window.location.host.toString()].join("");
 }
 
 // Get the fundamental object of the given type
-function getStdObj(typeName)
-{
-    switch(typeName)
-    {
+function getStdObj(typeName) {
+    switch (typeName) {
         case "object": return Object;
         case "boolean": return Boolean;
         case "number": return Number;
@@ -188,8 +173,7 @@ function getStdObj(typeName)
 }
 
 // Return one variable, coerced to the type of another.
-function matchType(inVar, toMatch)
-{
+function matchType(inVar, toMatch) {
     return getStdObj(typeof toMatch)(inVar);
 }
 
@@ -197,13 +181,14 @@ function matchType(inVar, toMatch)
 // Looks for the specified attribute in each array entry, and adds an alias for
 // it at the top level.
 function indexArrayByAttr(inArray, attr) {
-    inArray.forEach( function(elem,ignore,arr){
+    inArray.forEach(function (elem, ignore, arr) {
         // Add a named alias to each entry.
         if (isValid(elem[attr]) && !isValid(arr[elem[attr]])) {
-            Object.defineProperty(arr, elem.id, { value : elem, enumerable:false });
+            Object.defineProperty(arr, elem.id, { value: elem, enumerable: false });
         }
-        else { console.log("Duplicate or missing "+attr+" attribute in array: "+elem[attr]); }
-}); }
+        else { console.log("Duplicate or missing " + attr + " attribute in array: " + elem[attr]); }
+    });
+}
 
 
 // Number format utility functions.
